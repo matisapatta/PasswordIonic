b1a6fa0d0d6b50a6360e4ad5a0d0c71d9fa85903
matisapatta <mati.sapatta@gmail.com>
2016-09-29 21:34:09 -0300

    refactor

---
+++ js/app.config.js
@@ -0,0 +1,56 @@
+/**
+ * Created by mati on 9/29/16.
+ */
+'use strict';
+
+angular.module('PasswordIonic')
+  .config(['$stateProvider','$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+      })
+
+      .state('app.search', {
+        url: '/search',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/search.html'
+          }
+        }
+      })
+
+      .state('app.browse', {
+        url: '/browse',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/browse.html'
+          }
+        }
+      })
+      .state('app.playlists', {
+        url: '/playlists',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlists.html',
+            controller: 'PlaylistsCtrl'
+          }
+        }
+      })
+
+      .state('app.single', {
+        url: '/playlists/:playlistId',
+        views: {
+          'menuContent': {
+            templateUrl: 'templates/playlist.html',
+            controller: 'PlaylistCtrl'
+          }
+        }
+      });
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/playlists');
+  }]);
---
+++ js/app.module.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mati on 9/29/16.
+ */
+'use strict';
+angular.
+  module('PasswordIonic', [
+    'ionic',
+    'starter.controllers'
+  ]);
